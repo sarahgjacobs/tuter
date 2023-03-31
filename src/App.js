9be0d471e864a50c04caac4c5233e2241710b3e2
Sarah Jacobs <sgjacobs14@gmail.com>
2023-03-31 16:06:21 -0400

    added login functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+//Make sure the user is logged in
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
---
+++ utils/queries.js
@@ -0,0 +1,16 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      tutor {
+        description
+        link
+        comments
+      }
+    }
+  }
+`;
